50c3803509d6aa3b942634be1173edbc94bed427
borisboeri <boris.boeri@gmail.com>
2018-10-15 11:54:55 +0200

    gradient descent

--- model_tf.py
+++ model_tf.py
@@ -40,7 +40,7 @@ def model(images, is_training, scope_name, reuse=False):
         # Input Tensor Shape: [batch_size, 28, 28, 32]
         # Output Tensor Shape: [batch_size, 14, 14, 32]
         pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-        feat_dict['pool1'] = conv1
+        feat_dict['pool1'] = pool1
         
         # Convolutional Layer #2
         # Computes 64 features using a 5x5 filter.
@@ -49,27 +49,27 @@ def model(images, is_training, scope_name, reuse=False):
         # Output Tensor Shape: [batch_size, 14, 14, 64]
         conv2 = tf.layers.conv2d( inputs=pool1, filters=64, kernel_size=[5, 5],
               padding="same", activation=tf.nn.relu)
-        feat_dict['conv2'] = conv1
+        feat_dict['conv2'] = conv2
         
         # Pooling Layer #2
         # Second max pooling layer with a 2x2 filter and stride of 2
         # Input Tensor Shape: [batch_size, 14, 14, 64]
         # Output Tensor Shape: [batch_size, 7, 7, 64]
         pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-        feat_dict['pool2'] = conv1
+        feat_dict['pool2'] = pool2
         
         # Flatten tensor into a batch of vectors
         # Input Tensor Shape: [batch_size, 7, 7, 64]
         # Output Tensor Shape: [batch_size, 7 * 7 * 64]
         pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-        feat_dict['pool2_flat'] = conv1
+        feat_dict['pool2_flat'] = pool2_flat
         
         # Dense Layer
         # Densely connected layer with 1024 neurons
         # Input Tensor Shape: [batch_size, 7 * 7 * 64]
         # Output Tensor Shape: [batch_size, 1024]
         dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-        feat_dict['dense'] = conv1
+        feat_dict['dense'] = dense
        
        # Add dropout operation; 0.6 probability that element will be kept
         dropout = tf.layers.dropout( inputs=dense, rate=0.4, training=is_training)
